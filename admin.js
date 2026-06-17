a18f867f6fbf728d0b67f6abe5ec07be53fd6e86
mustaphaabdulsalamshugaba-star <mustaphaabdulsalamshugaba@gmail.com>
2026-06-17 22:01:12 +0100

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,74 @@
+import { db } from "./firebase.js";
+
+import {
+collection,
+getDocs,
+deleteDoc,
+doc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const ordersDiv = document.getElementById("orders");
+
+async function loadOrders(){
+
+ordersDiv.innerHTML = "<h3>Loading Orders...</h3>";
+
+const querySnapshot = await getDocs(
+collection(db,"orders")
+);
+
+let output = "";
+
+querySnapshot.forEach((documentData)=>{
+
+const order = documentData.data();
+
+output += `
+
+<div class="order">
+
+<h3>${order.name}</h3>
+
+<p><strong>Phone:</strong> ${order.phone}</p>
+
+<p><strong>Address:</strong> ${order.address}</p>
+
+<p><strong>Date:</strong> ${order.date}</p>
+
+<p><strong>Products:</strong></p>
+
+<pre>${JSON.stringify(order.products,null,2)}</pre>
+
+<button
+class="delete-btn"
+onclick="deleteOrder('${documentData.id}')">
+
+Delete Order
+
+</button>
+
+</div>
+
+`;
+
+});
+
+ordersDiv.innerHTML = output;
+
+}
+
+window.deleteOrder = async function(id){
+
+if(confirm("Delete this order?")){
+
+await deleteDoc(
+doc(db,"orders",id)
+);
+
+loadOrders();
+
+}
+
+}
+
+loadOrders();
